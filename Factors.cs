d0376d9646aa7503b2d66d750098524f156e3966
Mujeebanwar1 <mujeebanwar76@gmail.com>
2023-06-11 14:12:19 +0530

    createrd evenodd

---
+++ Factors.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgramm
+{
+    internal class Factors
+    {
+
+        public void Factor()
+        {
+            Console.WriteLine("Enter the Number to Calculate is Factors :");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The Factors are : ");
+            for (int i = 2; i * i <= number; i++)
+            {
+                while (number % i == 0)
+                {
+                    Console.WriteLine(i);
+                    number /= i;
+                }
+            }
+            if (number > 1)
+            {
+                Console.WriteLine(number);
+            }
+            Console.WriteLine("Those are the Factors ");
+        }
+    }
+}
